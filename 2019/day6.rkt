d26eae735eec7a41dd008c293edabf4a99a930ba
Kees-Jochem Wehrmeijer <keesjochem@hencq.nl>
2019-12-06 08:36:52 -0800

    Day 6 using graph library instead

--- 2019/day6.rkt
+++ 2019/day6.rkt
@@ -1,11 +1,31 @@
 #lang racket
 
-(require rackunit
-         threading
-         "../utils.rkt")
+(require graph
+         rackunit)
+
+(define (input->dists input from)
+  (define-values (dists _)
+    (bfs
+     (unweighted-graph/undirected (map (lambda (s)
+                                         (string-split s ")"))
+                                       (string-split input "\n")))
+     from))
+  dists)
+
+(define (part1 input)
+  (apply + (hash-values (input->dists input "COM"))))
+
+(define (part2 s)
+  (define dists (input->dists s "YOU"))
+  (- (hash-ref dists "SAN") 2))
+
+(define input (file->string "input6.txt"))
+(part1 input)
+(part2 input)
+
 
 (module+ test
-  (define test "COM)B
+  (define test1 "COM)B
 B)C
 C)D
 D)E
@@ -16,53 +36,7 @@ D)I
 E)J
 J)K
 K)L")
-  (check-eq? (part1 test) 42))
-
-
-(define (make-digraph input)
-  (define orbits (map (lambda (s) (string-split s ")")) (string-split input "\n")))
-  (for/fold ([graph (hash)])
-            ([o (in-list orbits)])
-    (hash-update graph (car o) (curry cons (cadr o)) '())))
-
-(define (checksum graph)
-  (let loop ([node "COM"]
-             [count 0])
-    (if (hash-has-key? graph node)
-        (foldl (lambda (n total)
-                 (+ total (loop n (add1 count)))) count (hash-ref graph node))
-        count)))
-
-(define (part1 s)
-  (checksum (make-digraph s)))
-
-(define (make-graph input)
-  (define orbits (map (lambda (s) (string-split s ")")) (string-split input "\n")))
-  (for/fold ([graph (hash)])
-            ([o (in-list orbits)])
-    (~> graph
-        (hash-update (car o) (curry cons (cadr o)) '())
-        (hash-update (cadr o) (curry cons (car o)) '()))))
-
-(define (calc-dists graph [root "YOU"])
-  (define q (deque))
-  (push-end! q (cons root 0))
-  (let loop ([dists (hash)])
-    (if (deque-empty? q)
-        dists
-        (match-let ([(cons n dist) (pop-front! q)])
-          (if (hash-has-key? dists n)
-              (loop dists)
-              (let ()
-                (for ([adj (in-list (hash-ref graph n))])
-                  (push-end! q (cons adj (add1 dist))))
-                (loop (hash-set dists n dist))))))))
-
-(define (part2 s)
-  (define dists (calc-dists (make-graph s)))
-  (- (hash-ref dists "SAN") 2))
-
-(module+ test
+    (check-eq? (part1 test1) 42)
   (define test2 "COM)B
 B)C
 C)D
@@ -77,8 +51,4 @@ K)L
 K)YOU
 I)SAN")
   (check-eq? (part2 test2) 4))
-
-(define input (file->string "input6.txt"))
-(part1 input)
-(part2 input)
 
