05e95fc0b9806ff6d191e1d47e773340419ad886
Kees-Jochem Wehrmeijer <keesjochem@hencq.nl>
2019-12-06 09:01:35 -0800

    Clean up intcode interpreter

--- intcode.rkt
+++ intcode.rkt
@@ -2,10 +2,9 @@
 
 (require rackunit)
 
-(provide run)
+(provide run!)
 
 ;; Some intcode utilities to (hopefully) help with later puzzles
-
 
 ;; A little mini assembler to help test things
 
@@ -34,7 +33,7 @@
   (define reg-map (for/hash ([c (in-list regs)]
                              [i (in-naturals (+ code-len (length consts)))])
                     (values c i)))
-  (append
+n  (append
    (for/list ([c (in-list code)])
      (cond
        [(eq? c 'add) 1]
@@ -65,128 +64,111 @@
 
 ;; Running intcode
 
-(define (input->vector input)
+(struct prog (instrs pc done) #:mutable #:transparent)
+
+(define (new-prog instrs)
+  (cond
+    [(list? instrs) (set! instrs (list->vector instrs))]
+    [(string? instrs) (set! instrs (string->ivector instrs))])
+  (prog instrs 0 #f))
+
+(define (string->ivector input)
   (list->vector
    (map (compose string->number string-trim) (string-split input ","))))
 
-(define (op vec pc fun modes)
-  (define a (vector-ref vec (+ pc 1)))
-  (define b (vector-ref vec (+ pc 2)))
-  (define c (vector-ref vec (+ pc 3)))
-  (vector-set! vec c (fun
-                      (if (= (first modes) 1)
-                          a
-                          (vector-ref vec a))
-                      (if (= (second modes) 1)
-                          b
-                          (vector-ref vec b))))
-  vec)
+(define (restart! p)
+  (set-prog-pc! p 0)
+  (set-prog-done! p #f))
 
-(define (input-op vec pc in modes)
-  (vector-set! vec (vector-ref vec (add1 pc)) (in))
-  vec)
+(define (get-cell p pc)
+  (vector-ref (prog-instrs p) pc))
 
-(define (output-op vec pc out modes)
-  (define param (vector-ref vec (add1 pc)))
-  (if (= (car modes) 1)
-      (out param)
-      (out (vector-ref vec param)))
-  vec)
+(define (get-param p i)
+  (vector-ref (prog-instrs p) (+ (prog-pc p) i)))
 
-(define (jump-op vec pc mod modes)
-  (define arg1 (let ([tmp (vector-ref vec (+ pc 1))])
-                 (if (= (first modes) 0)
-                     (vector-ref vec tmp)
-                     tmp)))
-  (define arg2 (let ([tmp (vector-ref vec (+ pc 2))])
-                 (if (= (second modes) 0)
-                     (vector-ref vec tmp)
-                     tmp)))
-  (if (mod (= arg1 0))
-      arg2
-      (+ pc 3)))
+(define (get-param-val p i)
+  (define op (cur-instr p))
+  (define base (expt 10 (add1 i)))
+  (define mode (quotient (modulo op (* base 10)) base))
+  (define param (get-param p i))
+  (if (= mode 1)
+      param
+      (vector-ref (prog-instrs p) param)))
 
-(define (comp-op vec pc pred modes)
-  (define arg1 (let ([tmp (vector-ref vec (+ pc 1))])
-                 (if (= (first modes) 0)
-                     (vector-ref vec tmp)
-                     tmp)))
-  (define arg2 (let ([tmp (vector-ref vec (+ pc 2))])
-                 (if (= (second modes) 0)
-                     (vector-ref vec tmp)
-                     tmp)))
-  (vector-set! vec (vector-ref vec (+ pc 3))
-               (if (pred arg1 arg2) 1 0))
-  vec)
+(define (cur-instr p)
+  (vector-ref (prog-instrs p) (prog-pc p)))
 
-(module+ test
-  (define test-input "1,9,10,3,
-              2,3,11,0,
-              99,
-              30,40,50")
-  (define test-vec (input->vector test-input))
-  (op test-vec 0 + (list 0 0 0))
-  (check-eq? (vector-ref test-vec 3) 70)
-  (op test-vec 4 * (list 0 0 0))
-  (check-eq? (vector-ref test-vec 0) 3500))
+;; Get short opcode
+(define (opcode p)
+  (define long-code (cur-instr p))
+  (- long-code (* 100 (quotient long-code 100))))
 
-(define (run-prog prog in out)
-  (let loop ([pc 0])
-    (define-values (opcode modes) (get-modes (vector-ref prog pc) 3))
+(define (make-math-op fun)
+  (lambda (p)
+    (define op1 (get-param-val p 1))
+    (define op2 (get-param-val p 2))
+    (define dest (get-param p 3))
+    (vector-set! (prog-instrs p) dest (fun op1 op2))
+    (set-prog-pc! p (+ (prog-pc p) 4))))
+
+(define (make-jump-op pred)
+  (lambda (p)
+    (define x (get-param-val p 1))
+    (define jmp (get-param-val p 2))
+    (if (pred x)
+        (set-prog-pc! p jmp)
+        (set-prog-pc! p (+ (prog-pc p) 3)))))
+
+(define (make-comp-op comp)
+  (lambda (p)
+    (define a (get-param-val p 1))
+    (define b (get-param-val p 2))
+    (define dest (get-param p 3))
+    (define result (if (comp a b) 1 0))
+    (vector-set! (prog-instrs p) dest result)
+    (set-prog-pc! p (+ (prog-pc p) 4))))
+
+(define add! (make-math-op +))
+(define mul! (make-math-op *))
+(define jiz! (make-jump-op (curry = 0)))
+(define jnz! (make-jump-op (compose not (curry = 0))))
+(define cmplt! (make-comp-op <))
+(define cmpeq! (make-comp-op =))
+
+(define (inp! p in)
+  (vector-set! (prog-instrs p) (get-param p 1) (car in))
+  (set-prog-pc! p (+ (prog-pc p) 2)))
+
+(define (out! p out)
+  (define res (cons (get-param-val p 1) out))
+  (set-prog-pc! p (+ (prog-pc p) 2))
+  res)
+
+(define (run! prog [input '()])
+  (let loop ([in input]
+             [out '()])
+    (define op (opcode prog))
     (cond
-      [(= opcode 99) prog]
-      [(= opcode 1) (op prog pc + modes) (loop (+ pc 4))]
-      [(= opcode 2) (op prog pc * modes) (loop (+ pc 4))]
-      [(= opcode 3) (input-op prog pc in modes) (loop (+ pc 2))]
-      [(= opcode 4) (output-op prog pc out modes) (loop (+ pc 2))]
-      [(= opcode 5) (loop (jump-op prog pc not modes))]
-      [(= opcode 6) (loop (jump-op prog pc values modes))]
-      [(= opcode 7) (comp-op prog pc < modes) (loop (+ pc 4))]
-      [(= opcode 8) (comp-op prog pc = modes) (loop (+ pc 4))])))
+      [(prog-done prog) (void)]
+      [(= op 99) (set-prog-done! prog #t) (reverse out)]
+      [(= op 1) (add! prog) (loop in out)]
+      [(= op 2) (mul! prog) (loop in out)]
+      [(= op 3) (inp! prog in) (loop (cdr in) out)]
+      [(= op 4) (loop in (out! prog out))]
+      [(= op 5) (jnz! prog) (loop in out)]
+      [(= op 6) (jiz! prog) (loop in out)]
+      [(= op 7) (cmplt! prog) (loop in out)]
+      [(= op 8) (cmpeq! prog) (loop in out)])))
 
-(define (run-asm code in out)
-  (run-prog (asm code) in out))
-
-(define (run input in out)
-  (define prog (input->vector input))
-  (run-prog prog in out))
-
-(module+ test
-  (define (in) 1)
-  (define (out x) (void))
-  (check-equal? (run test-input in out) (vector 3500 9 10 70 2 3 11 0 99 30 40 50))
-  (check-equal? (run "1,0,0,0,99" in out) (vector 2 0 0 0 99))
-  (check-equal? (run "2, 4, 4, 5, 99, 0" in out) (vector 2 4 4 5 99 9801))
-  ;; (* (+ 1 2) (+ 3 4))
-  (check-eq? (vector-ref (run-asm
-                          (list 
-                           (add 1 2 (reg 1))
-                           (add 3 4 (reg 2))
-                           (mul (reg 1) (reg 2) (reg 3))
-                           halt)
-                          in out)
-                         19)
-             21))
-
-(define (get-modes op n)
-  (let loop ([op op]
-             [pos (expt 10 (add1 n))]
-             [modes '()])
-    (if (= pos 10)
-        (values op modes)
-        (let ([mode (quotient op pos)])
-          (loop (- op (* mode pos)) (/ pos 10) (cons mode modes))))))
 
 (module+ test
   (define input "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
 1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
 999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99")
-  (define output #f)
-  (define outfun (lambda (x) (set! output x)))
-  (define tmp (run input (lambda () 9) (lambda (x) (printf "> ~a~%" x))))
-  (check-eq? output 1001)
-  (set! tmp (run input (lambda () 8) (lambda (x) (printf "> ~a~%" x))))
-  (check-eq? output 1000)
-  (set! tmp (run input (lambda () 6) (lambda (x) (printf "> ~a~%" x))))
-  (check-eq? output 999))
+  (define p (new-prog input))
+  (check-eq? (car (run! p (list 9))) 1001)
+  (restart! p)
+  (check-eq? (car (run! p (list 8))) 1000)
+  (restart! p)
+  (check-eq? (car (run! p (list 7))) 999))
 
