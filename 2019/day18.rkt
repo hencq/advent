bcbaeaa3b6798da9b71934f33769c37c259999d0
Kees-Jochem Wehrmeijer <keesjochem@hencq.nl>
2019-12-25 16:32:01 -0800

    Day 18 - part 2
    Still pretty slow, but it works

--- 2019/day18.rkt
+++ 2019/day18.rkt
@@ -1,7 +1,8 @@
 #lang racket
 
 (require "../utils.rkt"
-         data/heap)
+         data/heap
+         threading)
 
 (define test0
   "#########
@@ -38,63 +39,30 @@
 ###g#h#i################
 ########################")
 
-(struct node (pos keys) #:transparent)
 
-(define (find-keys mapstr)
-  (define area (read-area mapstr #:ignore '(#\.)))
-  (define keys (map (compose car string->list)
-                    (regexp-match* "[a-z]" mapstr)))
-  (define (found-all? n)
-    (set-empty? (set-subtract keys (node-keys n))))
-  (define (wall? n)
-    (equal? (hash-ref area (node-pos n) #f) #\#))
-  (define (door? n)
-    (define sq (hash-ref area (node-pos n) #f))
-    (and (char? sq) (char>=? sq #\A) (char<=? sq #\Z)))
-  (define (can-enter? n)
-    (if (wall? n)
-        #f
-        (if (door? n)
-            (let ([needed (char-downcase (hash-ref area (node-pos n) #f))])
-              (set-member? (node-keys n) needed))
-            #t)))
-  (define (add-neighbors! n steps seen)
-    (define keyring (let ([sq (hash-ref area (node-pos n) #f)])
-                      (if (and (char? sq) (char>=? sq #\a) (char<=? sq #\z))
-                          (set-add (node-keys n) sq)
-                          (node-keys n))))
-    (for ([dx (in-list '(1 0 -1 0))]
-          [dy (in-list '(0 1 0 -1))])
-      (match-define (cons x y) (node-pos n))
-      (define adj (struct-copy node n
-                               [pos (cons (+ x dx) (+ y dy))]
-                               [keys keyring]))
-      (when (and (not (set-member? seen adj)) (not (wall? adj))
-                 (can-enter? adj))
-        (push-end! q (cons adj (add1 steps))))))
-  (define start (for/first ([(p t) (in-hash area)]
-                          #:when (equal? t #\@))
-                  p))
-  (define q (deque))
-  (push-end! q (cons (node start '()) 0))
-  (let walk ([seen (set)])
-    (if (deque-empty? q)
-        #f
-        (let* ([item (pop-front! q)]
-               [cur (car item)]
-               [steps (cdr item)])
-          (printf "@~a (~a): ~a~%" (node-pos cur) (node-keys cur)
-                  (hash-ref area (node-pos cur) #\.))
-          (cond
-            [(found-all? cur) (values (sub1 steps) (node-keys cur))]
-            [else (add-neighbors! cur steps seen) (walk (set-add seen cur))])))))
+(define test5 "###############
+#d.ABC.#.....a#
+######@#@######
+###############
+######@#@######
+#b.....#.....c#
+###############")
+
+(define test6 "#############
+#DcBa.#.GhKl#
+#.###@#@#I###
+#e#d#####j#k#
+###C#@#@###J#
+#fEbA.#.FgHi#
+#############")
 
 (define (distances area start)
   (define (square pos)
     (hash-ref area pos #f))
   (define (floor? sq)
     (or (equal? sq #\.)
-        (equal? sq #\@)))
+        (equal? sq #\@)
+        (and (char? sq) (char>=? sq #\0) (char<=? sq #\9))))
   (define (door? sq)
     (and (char? sq) (char-upper-case? sq)))
   (define (key? sq)
@@ -124,24 +92,29 @@
                         (loop (set-add seen pos) key-dists)]
             [else (loop (set-add seen pos) key-dists)])))))
 
+(define (number-entrances area)
+  (define counter (char->integer #\0))
+  (for/hash ([(pos sq) (in-hash area)])
+    (if (char=? sq #\@)
+        (let ()
+          (set! counter (add1 counter))
+          (values pos (integer->char counter)))
+        (values pos sq))))
 
-(define test-area (read-area test #:ignore '(#\#)))
-
-(distances test-area (cons 15 1))
-
-;(min-dists test-area)
-;(distances test-area (cons 15 1) '())
-
-(define (shortest-path mapstr)
-  (define area (read-area mapstr #:ignore '(#\#)))
+(define (shortest-path area)
+  (set! area (number-entrances area))
   (define keys (for/hash ([(pos sq) (in-hash area)]
                           #:when (or (char=? sq #\@)
+                                     (and (char? sq) (char>=? sq #\0) (char<=? sq #\9))
                                      (char-lower-case? sq)))
                  (values sq pos)))
   (define dists (for/hash ([(key pos) (in-hash keys)])
                   (values key (distances area pos))))
-  (define (adjacents from-keys)
-    (match-define (cons from keys) from-keys)
+  (define entrances (for/hash ([sq (in-list (hash-keys keys))]
+                               #:when (and (char? sq) (char>=? sq #\0) (char<=? sq #\9)))
+                      (values sq sq)))
+  
+  (define (adjacents from keys)
     (for*/list ([(to node) (in-hash (hash-ref dists from))]
                 [doors (in-value (car node))]
                 [dist (in-value (cdr node))]
@@ -149,25 +122,57 @@
                             (andmap (curry set-member? keys) doors)))
       (cons to dist)))
   
-  (let loop ([paths (hash (cons #\@ (set)) 0)])
+  (let loop ([paths (hash (cons entrances (set)) 0)])
     (define dists (for*/fold ([dists (hash)])
                              ([(path pathlen) (in-hash paths)]
-                              [adj (in-list (adjacents path))])
+                              [entrances (in-value (car path))]
+                              [keys (in-value (cdr path))]
+                              [(entrance last-pos) (in-hash entrances)]
+                              [adj (in-list (adjacents last-pos keys))])
                     (match-define (cons key dist) adj)
                     (define found (set-add (cdr path) key))
-                    (define newnode (cons key found))
+                    (define newnode (cons (hash-set entrances entrance key) found))
                     (define cached (hash-ref dists newnode #f))
                     (if (and cached (< cached (+ pathlen dist)))
                         dists
                         (hash-set dists newnode (+ pathlen dist)))))
+    
     (if (= 0 (hash-count dists))
         (apply min (hash-values paths))
         (loop dists))))
 
-(time (shortest-path test4))
+(define (part1 mapstr)
+  (shortest-path (read-area mapstr #:ignore '(#\#))))
+
+(define (part2-area input)
+  (define area (read-area input #:ignore '(#\#)))
+  (define entrance (for/first ([(pos sq) (in-hash area)]
+                               #:when (char=? sq #\@))
+                     pos))
+  (define x (car entrance))
+  (define y (cdr entrance))
+  (~> area
+      (hash-set entrance #\#)
+      (hash-set (cons (sub1 x) y) #\#)
+      (hash-set (cons (add1 x) y) #\#)
+      (hash-set (cons x (sub1 y)) #\#)
+      (hash-set (cons x (add1 y)) #\#)
+      (hash-set (cons (sub1 x) (sub1 y)) #\@)
+      (hash-set (cons (sub1 x) (add1 y)) #\@)
+      (hash-set (cons (add1 x) (sub1 y)) #\@)
+      (hash-set (cons (add1 x) (add1 y)) #\@)))
+
+(define (part2 input)
+  (define area (part2-area input))
+  (shortest-path area))
 
 (define input (file->string "input18.txt"))
-(time (shortest-path input))
+(time (part1 input))
+(time (part2 input))
+
+
+
+
 
 
 
