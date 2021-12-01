ba219e1896ae47ad51695a618d65038aa0a1976a
Kees-Jochem Wehrmeijer <info@hencq.nl>
2021-12-01 13:30:15 -0800

    Started 2021 in F#

---
+++ 2021/day1.fsx
@@ -0,0 +1,16 @@
+open System.IO
+
+//let test = File.ReadAllLines "test1.txt" |> Seq.map int
+let input = File.ReadAllLines "input1.txt" |> Seq.map int
+
+let increases depths =
+    Seq.pairwise depths |>
+    Seq.filter (fun (a, b) -> a < b) |>
+    Seq.length
+
+let window (depths : int seq) =
+    Seq.windowed 3 depths |>
+    Seq.map Array.sum 
+
+input |> increases
+input |> window |> increases
