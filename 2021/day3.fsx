8c76ca86cb88a9dfc96d945c089659e6fdb84724
Kees-Jochem Wehrmeijer <info@hencq.nl>
2021-12-03 01:07:45 -0800

    Day 3 clean up

--- 2021/day3.fsx
+++ 2021/day3.fsx
@@ -1,51 +1,50 @@
 open System.IO
 
-let test = [
-  "00100"
-  "11110"
-  "10110"
-  "10111"
-  "10101"
-  "01111"
-  "00111"
-  "11100"
-  "10000"
-  "11001"
-  "00010"
-  "01010"
-]
+let test =
+  [ "00100"
+    "11110"
+    "10110"
+    "10111"
+    "10101"
+    "01111"
+    "00111"
+    "11100"
+    "10000"
+    "11001"
+    "00010"
+    "01010" ]
 
-let input = File.ReadAllLines "input3.txt" |> Array.toList
+let input =
+  File.ReadAllLines "input3.txt" |> Array.toList
 
 // get a column of bits from the list of bytes by col number
-let getColumn (bytes : string list) col =
-  let folder state (byte : string) =
-    (byte[col])::state
-  List.fold folder [] bytes
+let getColumn (bytes: string list) col =
+  List.fold (fun state (byte : string) -> byte.[col] :: state) [] bytes
 
 // count the bits and return the most common
 let mostCommon col =
   let rec count col ones zeroes =
     match col with
     | [] -> if ones >= zeroes then '1' else '0'
-    | '1'::tl -> count tl (ones + 1) zeroes
-    | '0'::tl -> count tl ones (zeroes + 1)
+    | '1' :: tl -> count tl (ones + 1) zeroes
+    | '0' :: tl -> count tl ones (zeroes + 1)
   count col 0 0
 
 let leastCommon col =
   if mostCommon col = '1' then '0' else '1'
 
-// get each column and determine whether it's 1 or 0 
-let collectBits criteria (bytes : string list) =
-  let cols = String.length bytes[0]
-  [for i = 0 to cols - 1 do
-     let col = getColumn bytes i
-     yield criteria col]
+// get each column and determine whether it's 1 or 0
+let collectBits criteria (bytes: string list) =
+  let cols = String.length (bytes.[0])
+  List.init
+    cols
+    (fun i ->
+      let col = getColumn bytes i
+      criteria col)
   |> Array.ofList
   |> System.String
   |> fun s -> System.Convert.ToInt32(s, 2)
-    
-collectBits mostCommon test
+
 let gamma = collectBits mostCommon
 let epsilon = collectBits leastCommon
 
@@ -54,19 +53,23 @@ let part1 bytes = (gamma bytes) * (epsilon bytes)
 part1 input
 
 // filter the bytes based on the given criteria
-let filterBytes criteria (bytes : string list) =
-  let cols = String.length bytes[0]
+let filterBytes criteria (bytes: string list) =
+  let cols = String.length (bytes.[0])
+
   let rec filter colnum bytes =
-    if colnum = cols then bytes 
-    elif List.length bytes = 1 then bytes
+    if colnum = cols then
+      bytes
+    elif List.length bytes = 1 then
+      bytes
     else
       let col = getColumn bytes colnum
       let target = criteria col
+
       bytes
-      |> List.filter (fun byte ->
-                            byte[colnum] = target)
+      |> List.filter (fun byte -> byte.[colnum] = target)
       |> filter (colnum + 1)
-  System.Convert.ToInt32((filter 0 bytes)[0], 2)
+
+  System.Convert.ToInt32((filter 0 bytes).[0], 2)
 
 let oxygen = filterBytes mostCommon
 let co2 = filterBytes leastCommon
